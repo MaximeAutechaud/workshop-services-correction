dc9c4fb04b98682fddc8c98fc058b96b784b3cfc
Maxime Autechaud <maxime.autechaud@gmail.com>
2020-11-10 20:08:12 +0100

    oops

--- src/Service/ChangeOptimizer/Change.php
+++ src/Service/ChangeOptimizer/Change.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace App\Service\ChangeOptimizer;
+
+class Change
+{
+    /**
+     * @var integer
+     */
+    private $coin2;
+    /**
+     * @var integer
+     */
+    private $bill5;
+    /**
+     * @var integer
+     */
+    private $bill10;
+
+    public function __construct(int $coin2,int $bill5,int $bill10)
+    {
+        $this->coin2 = $coin2;
+        $this->bill5 = $bill5;
+        $this->bill10 = $bill10;
+    }
+}
--- src/Service/ChangeOptimizer/ChangeOptimizer.php
+++ src/Service/ChangeOptimizer/ChangeOptimizer.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\Service\ChangeOptimizer;
+
+class ChangeOptimizer
+{
+
+    public function getOptimalChange(int $amount)
+    {
+        $coin2 = $bill5 = $bill10 = 0;
+        if ($amount == 1 || $amount == 3)
+            return false;
+        if ($amount % 2 !== 0){
+            $bill5 += 1;
+            $amount -= 5;
+        }
+        if ($amount >= 10) {
+            $bill10 = floor($amount / 10);
+            $amount -= $bill10 * 10;
+        }
+        $coin2 = $amount / 2;
+
+        return new Change($coin2, $bill5, $bill10);
+
+    }
+
+}
